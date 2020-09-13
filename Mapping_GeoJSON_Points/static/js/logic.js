a717a268ad96526f75a5edaa3e0e3f318288b1fb
scottja312 <64980711+scottja312@users.noreply.github.com>
2020-09-13 15:39:54 -0400

    finalized pulling GeoJson data into map.

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -2,51 +2,7 @@
 console.log("working");
 
 // Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
-
-//  Add a marker to the map for Los Angeles, California (test).
-// let marker = L.marker([34.0522, -118.2437]).addTo(map);
-// Changed marker to a circle. 
-//L.circle([34.0522, -118.2437], {
-  //radius: 100
-//}).addTo(map);
-
-// Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
-
-// Grabbing our GeoJSON data.
-L.geoJSON(sanFranAirport, {
-  // We turn each feature into a marker on the map (using onEachFeature).
-  onEachFeature: function(feature, layer) {
-    console.log(layer);
-    layer.bindPopup();
-  }
-}).addTo(map);
-
-// Create the map object with a center and zoom level (alternative to setView()).
-//let map = L.map("mapid", {
-    //center: [
-      //40.7, -94.5
-    //],
-    //zoom: 4
-  //});
+let map = L.map('mapid').setView([30, 30], 2);
 
 // Create tile layer that will be the background of our map.
 // Replace tile layer with dark background.
@@ -55,6 +11,15 @@ attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStree
     maxZoom: 18,
     accessToken: API_KEY
 });
-
 // Add "graymap" tile layer to the map. 
 streets.addTo(map);
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/scottja312/Mapping_Earthquakes/master/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJson(data).addTo(map);
+});
