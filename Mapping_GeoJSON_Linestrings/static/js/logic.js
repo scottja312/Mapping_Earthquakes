486e65183859bc53d1f8665aa23e46afd8ac423e
scottja312 <64980711+scottja312@users.noreply.github.com>
2020-09-13 16:26:53 -0400

    finalized geojson linestrings plot

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -25,7 +25,7 @@ let baseMaps = {
 let map = L.map('mapid', {
 	center: [44.0, -80.0],
 	zoom: 2,
-	layers: [streets]
+	layers: [light]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -34,18 +34,25 @@ L.control.layers(baseMaps).addTo(map);
 // Accessing the airport GeoJSON URL
 let torontoData = "https://raw.githubusercontent.com/scottja312/Mapping_Earthquakes/master/torontoRoutes.json";
 
+// Create a style for the lines.
+let myStyle = {
+	color: "#ffffa1",
+	weight: 2
+}
+
 // Grabbing our GeoJSON data.
 d3.json(torontoData).then(function(data) {
   console.log(data);
 
   // Creating a GeoJSON layer with the retrieved data.
 L.geoJson(data, {
-  color:"#ffffa1",
-  weight: 2,
+  //color:"#ffffa1",
+ //weight: 2,
+  style: myStyle,
   onEachFeature: function(feature, layer) {
     layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: "
     + feature.properties.dst + "</h3>");  
-}
+  }
 })
 .addTo(map);
 });
