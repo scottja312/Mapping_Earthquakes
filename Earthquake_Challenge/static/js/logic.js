1f4f9e0983d46c6a777de0a216aacf60602ff8a0
scottja312 <64980711+scottja312@users.noreply.github.com>
2020-09-15 11:04:03 -0400

    updated index.html for techtonic plate logic.js file.

--- Earthquake_Challenge/static/js/logic.js
+++ Earthquake_Challenge/static/js/logic.js
@@ -1,15 +1,22 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create light view tile layer and default map background. 
+// Create streets view tile layer and default map background. 
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
-// Create a dark view tile layer option for the map.
+// Create a satellite view tile layer option for the map.
 let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a satellite view tile layer option for the map.
+let Light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -19,6 +26,7 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
 let baseMaps = {
   "Streets": streets,
   "Satellite Streets": satelliteStreets,
+  "Light": light
 };
 
 // Create the earthquake layer for our map.
@@ -32,7 +40,7 @@ let overlays = {
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-	center: [39.5, -98.5],
+	center: [40.7, -94.5],
 	zoom: 3,
 	layers: [streets]
 })
@@ -133,4 +141,10 @@ pointToLayer: function(feature, latlng) {
 
   // Add legend layer to map. 
   legend.addTo(map);
-  });
+
+  // Place d3.json() call for the tectonic plate data.
+  d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+
+
+
+});
