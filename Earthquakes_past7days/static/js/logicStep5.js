c5351929357fd17de9677124405c8ad01b208c2c
scottja312 <64980711+scottja312@users.noreply.github.com>
2020-09-15 10:02:36 -0400

    finalized code for showing legend on the earthquake map.

--- Earthquakes_past7days/static/js/logicStep5.js
+++ Earthquakes_past7days/static/js/logicStep5.js
@@ -110,27 +110,27 @@ pointToLayer: function(feature, latlng) {
 
   // Add details for the legend.
   legend.onAdd = function() {
-      let div = L.DomUtil.create('div', 'info legend');
-      const magnitudes = [0, 1, 2, 3, 4, 5];
-      const colors = [
-        "#98ee00",
-        "#d4ee00",
-        "#eecc00",
-        "#ee9c00",
-        "#ea822c",
-        "#ea2c2c"
-      ];
-  };
-  
-  // Loop through our density intervals and generate a label with a colored square for each interval
-      for (var i = 0; i < magnitudes.length; i++) {
-          console.log(colors[i]);
-          div.innerHTML +=
-              '<i style="background:' + colors[i] + '"></i> ' +
-              magnitudes[i] + (magnitudes[i + 1] ? '&ndash;' + magnitudes[i + 1] + '<br>' : '+');
-      }
+     let div = L.DomUtil.create('div', 'info legend');
+        const magnitudes = [0, 1, 2, 3, 4, 5];
+        const colors = [
+            "#98ee00",
+            "#d4ee00",
+            "#eecc00",
+            "#ee9c00",
+            "#ea822c",
+            "#ea2c2c"
+          ];
+          
+          // Loop through our density intervals and generate a label with a colored square for each interval
+    for ( var i = 0; i < magnitudes.length; i++) {
+        console.log(colors[i]);
+        div.innerHTML +=
+        '<i style="background:' + colors[i] + '"></i> ' +
+        magnitudes[i] + (magnitudes[i + 1] ? '&ndash;' + magnitudes[i + 1] + '<br>' : '+');
+        }
         return div;
-});
+    };
 
-// Add legend layer to map. 
-legend.addTo(map);
+  // Add legend layer to map. 
+  legend.addTo(map);
+  });
